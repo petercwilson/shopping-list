411d2e867ee9c6bdf2e05e1587c3b2969ef7fb36
Peter Wilson <pcwilson19@gmail.com>
2019-03-14 10:53:23 -0700

    add item and checked item working

--- index.js
+++ index.js
@@ -3,18 +3,39 @@
 // Use DOM manipulation and traversal to dynamically add and remove HTML elements and apply styles
 // Use this and event delegation
 // You must use a CDN-hosted verion of jQuery
+
 // Hint: you may find it helpful to read up on and use the following jQuery methods:
 // .submit(), preventDefault(), toggleClass(), and closest()
 
-// enter items they need to purchase by entering text and hitting "return" or clicking the "add item" button
-// need an event listener on the submit button for both mouse click and keyboard press and add them to the list
+// permanently remove items from this list
+// this is a function that uses an event listener on the submit form button
+// next it stops the default form behavior
+// the variable results is equal to the value that is passed to the input on the add item click
+// then append a li and the rest of the html with the result of the submit button
+// I don't believe this is the correct way to add html to the DOM but it worked
+
 $(function() {
     $('#js-shopping-list-form').submit(function(event) {
         event.preventDefault();
-        $('.shopping-list').append(`event.currentTarget's text is: ${$(event.currentTarget).text()}`);
+        const results = $(this).find('input[name="shopping-list-entry"]').val();
+        $('.shopping-list').append(
+        '<li>' + '<span class="shopping-item">' + results + 
+        '</span>' + '<div class="shopping-item-controls">' + 
+        '<button class="shopping-item-toggle">' + '<span class="button-label">' 
+        + 'check' + '</span>' + '</button>' + ' ' +
+        '<button class="shopping-item-delete">' + '<span class="button-label">' 
+        + 'delete' + '</span>' + '</button>' + '</div>' +
+        '</li>');
+    });
 });
 
 
 // check and uncheck items on the list by clicking the "check" button
+// use toggleClass to toggle and delete each li added with their respective class
 
-// permanently remove items from this list
+$(function() {
+    $('ul').on('click', 'li', function(event) {
+        $(this).toggleClass("shopping-item__checked");
+        //this.remove();
+    });
+});
